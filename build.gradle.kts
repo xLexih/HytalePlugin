efdbcc37c3e3155086844e8f31efab4b3cd9837a
laptop <laptop@lexih.dev>
2026-01-18 18:20:24 +0100

    fix!: updated to newer version of the plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 @file:Suppress("PropertyName", "SpellCheckingInspection")
 
+import app.ultradev.hytalegradle.manifest.ManifestExtension
+
+
 plugins {
     kotlin("jvm") version "2.3.0"
     id("com.gradleup.shadow") version "9.3.1"
-    id("app.ultradev.hytalegradle") version "1.5.3"
+    id("app.ultradev.hytalegradle") version "1.6.7"
 }
 
 group = "dev.lexih.mod"
@@ -13,8 +16,18 @@ repositories { mavenCentral() }
 dependencies { testImplementation(kotlin("test")) }
 
 hytale {
-    patchline = "release"
+    patchline = "pre-release"
     allowOp = true
+    manifest {
+        group = "Lexih"
+        name = "TestPlugin"
+        mainClass = "dev.lexih.mod.TestPlugin"
+        description = "Example plugin"
+        website = "https://lexih.dev"
+        author("Lexih")
+        version = project.version.toString()
+        includesAssetPack = true
+    }
 }
 
 kotlin { jvmToolchain(25) }
