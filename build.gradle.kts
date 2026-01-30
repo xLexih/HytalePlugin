101710754c3c311fcbab33af02d5123d2600baee
Lexih <67009568+xLexih@users.noreply.github.com>
2026-01-30 18:25:19 +0100

    fix: version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 plugins {
     kotlin("jvm") version "2.3.0"
     id("com.gradleup.shadow") version "9.3.1"
-    id("app.ultradev.hytalegradle") version "2.0.1"
+    id("app.ultradev.hytalegradle") version "2.0.2"
 }
 
 group = "dev.lexih.mod"
